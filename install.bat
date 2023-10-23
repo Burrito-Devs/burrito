a6b713af5cadd399127fa10aafe9f46716c6ee8e
The Bernie <thebernie.eve@gmail.com>
2023-10-22 17:13:05 -0700

    Fix install script

--- install.bat
+++ install.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
 set burrito_dir=%HOMEPATH%\.burrito
 mkdir %burrito_dir%
-xcopy data\sounds %burrito_dir%
-copy data\systems.json %burrito_dir%
-copy burrito.exe %HOMEPATH%
+mkdir %burrito_dir%\sounds
+xcopy data\sounds %burrito_dir%\sounds /E /I /Y 
+copy /Y data\systems.json %burrito_dir%
+copy /Y burrito.exe %HOMEPATH%
