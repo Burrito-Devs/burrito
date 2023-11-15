8245139ae346efb2e43b9bae85556522f0f9eea0
The Bernie <thebernie.eve@gmail.com>
2023-11-15 10:46:32 -0800

    New config values populate cfg file automatically

--- src/burrito/serde_utils.rs
+++ src/burrito/serde_utils.rs
@@ -1,10 +1,9 @@
-use std::{fs::File, io::{BufWriter, BufReader}};
+use std::{fs::File, io::{BufWriter, BufReader, Write}};
 
 use crate::burrito::utils;
 
 // TODO: Refactor bounds for T into a new trait
 pub fn read_or_create_default_data_struct<T: for<'a> serde::Deserialize<'a> + serde::Serialize + Default + Clone>(path: &str, filename: &str) -> T {
-    // TODO Prefer returning default to panicking
     let mut path_builder = utils::get_burrito_dir();
     path_builder.push_str("/");
     path_builder.push_str(path);
@@ -22,13 +21,19 @@ pub fn read_or_create_default_data_struct<T: for<'a> serde::Deserialize<'a> + se
         let writer = BufWriter::new(def_file);
         serde_json::to_writer_pretty(writer, &def_value).expect("Failed to write default data file");
     }
-    else {// TODO: HERE! this isn't working. New values do not get added to config file
-        let f = File::open(path_builder).expect("Unable to open data file");
-        let f_w = f.try_clone().unwrap();
+    else {
+        let f = File::open(&path_builder).expect("Unable to open data file");
         let reader = BufReader::new(f);
         let loaded_struct: T = serde_json::from_reader(reader).expect("Invalid struct");
-        let writer = BufWriter::new(f_w);
-        serde_json::to_writer_pretty(writer, &loaded_struct).expect("Failed to update data file");
+        let mut f_w = std::fs::OpenOptions::new()
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .open(&path_builder)
+            .expect("Failed to save context file");
+        f_w.write_all(serde_json::to_string_pretty(&loaded_struct)
+                .expect("Failed to serialize context").as_bytes())
+            .expect("Failed to write context to file");
         ret_value = loaded_struct;
     }
     return ret_value;
