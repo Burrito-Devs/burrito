b8b6eb000f3359bb78d1f0b8aa51f1be6c18c0c7
The Bernie <thebernie.eve@gmail.com>
2023-12-28 19:11:39 -0800

    Implement comparisons

--- src/burrito/log_event.rs
+++ src/burrito/log_event.rs
@@ -16,8 +16,8 @@ pub struct LogEvent {
 }
 
 impl LogEvent {
-    fn add_metadata(&mut self, k: impl ToString, v: impl ToString) {
-        self.event_metadata.put(k, v);
+    fn add_metadata(&mut self, k: impl ToString, v: impl ToString, t: LogEventMetadataType) {
+        self.event_metadata.put(k, v, t);
     }
 }
 
@@ -40,7 +40,39 @@ pub enum EventType {
 
 #[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
 pub struct LogEventMetadata {
-    metadata: HashMap<String, String>,
+    metadata: HashMap<String, LogEventMetadataField>,
+}
+
+#[derive(Clone, Copy, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
+pub enum LogEventMetadataType {
+    U32,
+    I32,
+    U64,
+    I64,
+    F32,
+    F64,
+    String,
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
+pub struct LogEventMetadataField {
+    data: String,
+    data_type: LogEventMetadataType,
+}
+
+impl LogEventMetadataField {
+    pub fn new(data: impl ToString, data_type: LogEventMetadataType) -> Self {
+        LogEventMetadataField {
+            data: data.to_string(),
+            data_type,
+        }
+    }
+    pub fn get_data(&self) -> &String {
+        return &self.data;
+    }
+    pub fn get_data_type(&self) -> LogEventMetadataType {
+        return self.data_type;
+    }
 }
 
 impl LogEventMetadata {
@@ -49,25 +81,26 @@ impl LogEventMetadata {
     }
     pub fn get<K: Borrow<str>, T: FromStr>(&self, key: K) -> Option<T> {
         if let Some(value) = self.metadata.get(key.borrow()) {
-            return value.as_str().parse().ok();
+            return value.get_data().as_str().parse().ok();
         }
         None
     }
-    pub fn put(&mut self, key: impl ToString, value: impl ToString) {
-        self.metadata.insert(key.to_string(), value.to_string());
+    pub fn put(&mut self, key: impl ToString, value: impl ToString, data_type: LogEventMetadataType) {
+        self.metadata.insert(key.to_string(), LogEventMetadataField::new(value.to_string(), data_type));
     }
 }
 
 mod test {
+
     #[test]
     fn test_test_test() {
-        use crate::burrito::log_event::LogEventMetadata;
+        use crate::burrito::log_event::{LogEventMetadata, LogEventMetadataType};
 
         let mut uut = LogEventMetadata::new();
         assert_eq!(None, uut.get::<&str, String>("does not exist"));
-        uut.put("u32", 5u32);
-        uut.put("f64".to_string(), 0.25f64);
-        uut.put("String", "My String");
+        uut.put("u32", 5u32, LogEventMetadataType::U32);
+        uut.put("f64".to_string(), 0.25f64, LogEventMetadataType::F64);
+        uut.put("String", "My String", LogEventMetadataType::String);
         assert_eq!(None, uut.get::<String, i64>("does not exist".to_string()));
         assert_eq!(Some(5u32), uut.get("u32"));
         assert_eq!(Some(0.25f64), uut.get("f64"));
--- src/burrito/rules.rs
+++ src/burrito/rules.rs
@@ -1,14 +1,99 @@
+use std::cmp::Ordering;
+
 use serde_derive::{Deserialize, Serialize};
+
+use super::log_event::{LogEventMetadataField, LogEventMetadataType};
 
 pub trait Comparable: Eq + Ord + PartialEq + PartialOrd {}
 impl<T: Eq + Ord + PartialEq + PartialOrd> Comparable for T {}
 
-#[derive(Clone, Copy, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
+#[derive(Clone, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
 pub struct EventRule {
+    metadata_tag: String,
     rule: ComparisonRule,
+    ref_value: String,
 }
 
+pub fn compare(data_field1: LogEventMetadataField, data_field2: LogEventMetadataField, cmp_type: ComparisonRule) -> bool {
+    let ordering = data_field_ord(data_field1, data_field2);
+    match cmp_type {
+        ComparisonRule::LessThan => ordering == Ordering::Less,
+        ComparisonRule::LessThanOrEqualTo => (ordering == Ordering::Less) || (ordering == Ordering::Equal),
+        ComparisonRule::NotEqualTo => ordering != Ordering::Equal,
+        ComparisonRule::EqualTo => ordering == Ordering::Equal,
+        ComparisonRule::GreaterThanOrEqualTo => (ordering == Ordering::Greater) || (ordering == Ordering::Equal),
+        ComparisonRule::GreaterThan => ordering == Ordering::Greater,
+    }
+}
 
+fn data_field_ord(data_field1: LogEventMetadataField, data_field2: LogEventMetadataField) -> Ordering {
+    // Types are not equal, use String ordering
+    if data_field1.get_data_type() != data_field2.get_data_type() {
+        data_field1.get_data().cmp(data_field2.get_data())
+    }
+    // Types are the same, use typed ordering
+    else {
+        match data_field1.get_data_type() {
+            LogEventMetadataType::U32 => {
+                let value1 = data_field1.get_data().parse::<u32>().unwrap();
+                let value2 = data_field2.get_data().parse::<u32>().unwrap();
+                value1.cmp(&value2)
+            },
+            LogEventMetadataType::I32 => {
+                let value1 = data_field1.get_data().parse::<i32>().unwrap();
+                let value2 = data_field2.get_data().parse::<i32>().unwrap();
+                value1.cmp(&value2)
+            },
+            LogEventMetadataType::U64 => {
+                let value1 = data_field1.get_data().parse::<u64>().unwrap();
+                let value2 = data_field2.get_data().parse::<u64>().unwrap();
+                value1.cmp(&value2)
+            },
+            LogEventMetadataType::I64 => {
+                let value1 = data_field1.get_data().parse::<i64>().unwrap();
+                let value2 = data_field2.get_data().parse::<i64>().unwrap();
+                value1.cmp(&value2)
+            },
+            LogEventMetadataType::F32 => {
+                let value1 = data_field1.get_data().parse::<f32>().unwrap();
+                let value2 = data_field2.get_data().parse::<f32>().unwrap();
+                ord_f32(value1, value2)
+            },
+            LogEventMetadataType::F64 => {
+                let value1 = data_field1.get_data().parse::<f64>().unwrap();
+                let value2 = data_field2.get_data().parse::<f64>().unwrap();
+                ord_f64(value1, value2)
+            },
+            LogEventMetadataType::String => {
+                data_field1.cmp(&data_field2)
+            },
+        }
+    }
+}
+
+fn ord_f32(v1: f32, v2: f32) -> Ordering {
+    if v1 > v2 {
+        Ordering::Greater
+    }
+    else if v2 > v1 {
+        Ordering::Less
+    }
+    else {
+        Ordering::Equal
+    }
+}
+
+fn ord_f64(v1: f64, v2: f64) -> Ordering {
+    if v1 > v2 {
+        Ordering::Greater
+    }
+    else if v2 > v1 {
+        Ordering::Less
+    }
+    else {
+        Ordering::Equal
+    }
+}
 
 #[derive(Clone, Copy, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
 pub enum ComparisonRule {
@@ -25,7 +110,7 @@ mod test {
     #[test]
     fn test_test_test() {
         use chrono::Utc;    
-        use crate::burrito::log_event::{EventType, LogEventMetadata, LogEvent};
+        use crate::burrito::log_event::{EventType, LogEventMetadata, LogEventMetadataType, LogEvent};
 
         let mut uut = LogEvent {
             time: Utc::now(),
@@ -36,11 +121,10 @@ mod test {
             event_metadata: LogEventMetadata::default(),
         };
 
-        uut.event_metadata.put("distance", 5u32);
+        uut.event_metadata.put("distance", 5u32, LogEventMetadataType::U32);
         assert_eq!(Some(5u32), uut.event_metadata.get("distance"));
         let asdf: u32 = uut.event_metadata.get("distance").unwrap();
         assert_eq!(5u32, asdf);
-
     }
 
 }
